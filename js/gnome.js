0770cdd97a9d6f837c79c44ff28ff8908ab0dae7
Alexander Kindzerske <akindzerske@gmail.com>
2014-12-06 15:02:14 -0500

    refactored BPM calculations into one function

--- js/gnome.js
+++ js/gnome.js
@@ -3,48 +3,33 @@ function tempoTiming(current, previous, avg, saved) {
     this.previousTap = previous;
     this.avgBPM = avg;
     this.savedBPMs = saved;
-    this.cleanArray = function(actual) {
+    this.calculateNewBPM = function() {
+        var sum = 0;
         var newArray = new Array();
-        for(var i = 0; i<actual.length; i++){
-            if (actual[i]){
-                newArray.push(actual[i]);
+        var instanceBPM = Math.max(Math.min(Math.round(600 / (this.currentTap - this.previousTap) * 100), 240), 40);
+        console.log('Instance BPM calculated to be ' + instanceBPM);
+        this.previousTap = this.currentTap;
+        this.savedBPMs.pop();
+        this.savedBPMs.unshift(instanceBPM);
+        for(var i = 0; i<this.savedBPMs.length; i++){
+            if (this.savedBPMs[i]){
+                newArray.push(this.savedBPMs[i]);
+            }
         }
-      }
-      return newArray;
-    };
-    this.appendDropLast = function(newValue, valueSet) {
-        valueSet.pop();
-        valueSet.unshift(newValue);
-        return valueSet;
-    };
-    this.averageArr = function(arr) {
-        arr = this.cleanArray(arr);
-        var sum = arr.reduce(function(a, b) { return a + b });
-        var avg = sum / arr.length;
-        return Math.round(avg);
-    };
-    this.calculateBPM = function(previous, recent) {
-        var bpm = Math.max(Math.min(Math.round(600 / (recent - previous) * 100), 240), 40);
-        console.log('Received times ' + previous + ' & ' + recent);
-        console.log('Calculated BPM to be ' + bpm + ' (range restricted between 40-240)');
-        return bpm;
+        sum = newArray.reduce(function(a, b) { return a + b });
+        this.avgBPM = sum / newArray.length;
+        this.avgBPM = Math.round(this.avgBPM);
     };
 }
+var metronome = new tempoTiming(0, 0, 0, new Array(6));
 $(document).ready(function() {
-    var metronome = new tempoTiming(0, 0, 0, new Array(10));
     $("button#tap").click(function() {
         metronome.currentTap = Date.now();
         if (metronome.previousTap == 0) {
             metronome.previousTap = metronome.currentTap;
         } else {
-            console.log('A new tap was recorded at ' + metronome.currentTap);
-            var thisBPM = metronome.calculateBPM(metronome.previousTap, metronome.currentTap)
-            metronome.savedBPMs = metronome.appendDropLast(thisBPM, metronome.savedBPMs);
-            metronome.avgBPM = metronome.averageArr(metronome.savedBPMs);
-            console.log('Stored BPMs are ' + metronome.savedBPMs);
-            console.log('New BPM calculated to be ' + metronome.avgBPM);
+            metronome.calculateNewBPM();
             $("button#tap span").text(metronome.avgBPM);
-            metronome.previousTap = metronome.currentTap;
         }
     });
 });
